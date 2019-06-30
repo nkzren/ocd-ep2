15954dbd46369959da661f0606196b6e70adb701
Nicael Robmar <nicael.robmar@hotmail.com>
2019-06-30 20:52:53 -0300

    IRcome

---
+++ src/ocd/IR.java
@@ -0,0 +1,9 @@
+package ocd;
+
+class IR{
+    public IR(int[] vet){
+        //da posicao 0 ate 6 adiciona num atr string Opcode
+        //nas proximas 6 posicoes coloca numa string e converte pra int
+        //nas ultimas 6 faz o mesmo
+    }
+}
