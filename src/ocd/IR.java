61d75224d81ec9a6925be9b8580ed61e5343eec0
nkzren <renan.nakazawa0@gmail.com>
2019-07-01 14:14:34 -0300

    correção do IR

--- src/ocd/IR.java
+++ src/ocd/IR.java
@@ -1,20 +1,43 @@
 package ocd;
 
-class IR{
-	
-	public static String opcode;
-	public static int porta1;
-	public static int porta2;
-	
-    public IR(int[] vet){
-    	String fullCode = Util.getInstance().intArrayToString(vet);
-    	String porta1String = fullCode.substring(6, 12);
-    	String porta2String = fullCode.substring(12);
-    	
-    	opcode = fullCode.substring(0, 6);
-    	porta1 = Util.getInstance().binaryStringToInt(porta1String);
-    	porta2 = Util.getInstance().binaryStringToInt(porta2String);
-    }
-    
-    
+class IR {
+
+	private static String opcode;
+	private static int porta1;
+	private static int porta2;
+
+	public IR(int[] vet) {
+		String fullCode = Util.getInstance().intArrayToString(vet);
+		String porta1String = fullCode.substring(6, 12);
+		String porta2String = fullCode.substring(12);
+
+		opcode = fullCode.substring(0, 6);
+		porta1 = Util.getInstance().binaryStringToInt(porta1String);
+		porta2 = Util.getInstance().binaryStringToInt(porta2String);
+	}
+
+	public static String getOpcode() {
+		return opcode;
+	}
+
+	public static void setOpcode(String opcode) {
+		IR.opcode = opcode;
+	}
+
+	public static int getPorta1() {
+		return porta1;
+	}
+
+	public static void setPorta1(int porta1) {
+		IR.porta1 = porta1;
+	}
+
+	public static int getPorta2() {
+		return porta2;
+	}
+
+	public static void setPorta2(int porta2) {
+		IR.porta2 = porta2;
+	}
+
 }
