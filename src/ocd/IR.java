a1233c2100db38d206a5708fff28a7fb0962ddf8
nkzren <renan.nakazawa0@gmail.com>
2019-07-01 14:13:11 -0300

    correção do IR

--- src/ocd/IR.java
+++ src/ocd/IR.java
@@ -7,9 +7,11 @@ class IR{
 	public static int porta2;
 	
     public IR(int[] vet){
-    	opcode = Util.getInstance().intArrayToString(vet);
-    	String porta1String = opcode.substring(6, 12);
-    	String porta2String = opcode.substring(12);
+    	String fullCode = Util.getInstance().intArrayToString(vet);
+    	String porta1String = fullCode.substring(6, 12);
+    	String porta2String = fullCode.substring(12);
+    	
+    	opcode = fullCode.substring(0, 6);
     	porta1 = Util.getInstance().binaryStringToInt(porta1String);
     	porta2 = Util.getInstance().binaryStringToInt(porta2String);
     }
